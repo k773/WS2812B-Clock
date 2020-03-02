aa3899f2bbcc04818b09d021cb7d8ff5707a14e2
k773 <54436604+k773@users.noreply.github.com>
2020-03-02 21:37:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 //
 // Created by Alexander Galilov on 26.05.2019.
 // E-Mail: alexander.galilov@gmail.com
+#include "ws2812b/Led.h"
+#include "ws2812b/RGB.h"
 
-#include "main.h"
-#include "Effects.h"
-#include "Clock.h"
-
+int NUM_LEDS = 30;
 WS2812B::Led leds(0, NUM_LEDS);
 
+void setPixelColor(int pixelNumber, int r, int g, int b);
+
+void setPixelColor(int pixelNumber, int r, int g, int b){
+    WS2812B::RGB color = leds.getColor(pixelNumber);
+    bool r_ok = false;
+    bool g_ok = false;
+    bool b_ok = false;
+    while (true){
+        if (!r_ok){
+            if (color._r > r) color._r --;
+            else if (color._r < r) color._r++;
+            else r_ok = true;
+        }
+        if (!g_ok){
+            if (color._g > g) color._g --;
+            else if (color._g < g) color._g++;
+            else g_ok = true;
+        }
+        if (!b_ok){
+            if (color._b > b) color._b --;
+            else if (color._b < b) color._b++;
+            else b_ok = true;
+        }
+        leds.setColor(color, pixelNumber);
+        leds.show();
+        if (r_ok && g_ok && b_ok) return;
+    }
+}
+
 int main() {
-    for (;;) {
-        //RGBLoop();
-        //Sparkle(0xff, 0xff, 0xff, 50);
-        //SnowSparkle(0x10, 0x10, 0x10, 20, myRandom(100,1000));
-        //RunningLights(0xff,0xff,0x00, 50);
-        //rainbowCycle(5);
-        //Fire(55,120,50);
-        BouncingBalls(0xff,0,0, 3);
-        //meteorRain(0xff,0xff,0xff,4, 64, true, 30);
-        //showClock();
+    while (true) {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            setPixelColor(i, 255, 255, 255);
+        }
+        for (int i = NUM_LEDS - 1; i > 0; i--) {
+            setPixelColor(i, 0, 0, 0);
+        }
     }
     return 0;
 }
